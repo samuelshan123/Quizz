7908793f9133bd97bc6507975be1827dc522a2e3
samuelshan123 <samuelshan123@gmail.com>
2020-03-24 22:31:16 -0700

    Initial commit

---
+++ app/src/main/java/com/samuel/android_quizz/QAnsPage.java
@@ -0,0 +1,140 @@
+package com.samuel.android_quizz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.muddzdev.styleabletoast.StyleableToast;
+
+public class QAnsPage extends AppCompatActivity {
+    private QuestionLibrary mQuestionLibrary = new QuestionLibrary();
+
+    private TextView mScoreView;
+    private TextView mQuestionView;
+    private Button mButtonChoice1;
+    private Button mButtonChoice2;
+    private Button mButtonChoice3;
+    private Button quit;
+
+    private String mAnswer;
+    private int mScore = 0;
+    private int mQuestionNumber = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_q_ans_page);
+
+        mScoreView = (TextView)findViewById(R.id.score);
+        mQuestionView = (TextView)findViewById(R.id.question);
+        mButtonChoice1 = (Button)findViewById(R.id.choice1);
+        mButtonChoice2 = (Button)findViewById(R.id.choice2);
+        mButtonChoice3 = (Button)findViewById(R.id.choice3);
+        quit = (Button)findViewById(R.id.quit);
+
+        updateQuestion();
+
+
+
+        //Start of Button Listener for Button1
+        mButtonChoice1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //My logic for Button goes in here
+
+                if (mButtonChoice1.getText() == mAnswer){
+                    mScore = mScore + 1;
+                    updateScore(mScore);
+                    updateQuestion();
+                    //This line of code is optiona
+                    StyleableToast.makeText(QAnsPage.this, "Correct Answer", R.style.correct).show();
+
+                }else {
+                    StyleableToast.makeText(QAnsPage.this, "Wrong Answer", R.style.wrong).show();
+                    updateQuestion();
+                }
+            }
+        });
+
+        //End of Button Listener for Button1
+
+        //Start of Button Listener for Button2
+        mButtonChoice2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //My logic for Button goes in here
+
+                if (mButtonChoice2.getText() == mAnswer){
+                    mScore = mScore + 1;
+                    updateScore(mScore);
+                    updateQuestion();
+                    //This line of code is optiona
+                    StyleableToast.makeText(QAnsPage.this, "Correct Answer", R.style.correct).show();
+
+
+                }else {
+                    StyleableToast.makeText(QAnsPage.this, "Wrong Answer", R.style.wrong).show();
+                    updateQuestion();
+                }
+            }
+        });
+
+        //End of Button Listener for Button2
+
+
+        //Start of Button Listener for Button3
+        mButtonChoice3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //My logic for Button goes in here
+
+                if (mButtonChoice3.getText() == mAnswer){
+                    mScore = mScore + 1;
+                    updateScore(mScore);
+                    updateQuestion();
+                    //This line of code is optiona
+                    StyleableToast.makeText(QAnsPage.this, "Correct Answer", R.style.correct).show();
+
+
+                }else {
+                    StyleableToast.makeText(QAnsPage.this, "Wrong Answer", R.style.wrong).show();
+                    updateQuestion();
+                }
+            }
+        });
+
+        //End of Button Listener for Button3
+        quit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //My logic for Button goes in here
+
+               finishAffinity();
+            }
+        });
+
+    }
+
+
+    private void updateQuestion(){
+        mQuestionView.setText(mQuestionLibrary.getQuestion(mQuestionNumber));
+        mButtonChoice1.setText(mQuestionLibrary.getChoice1(mQuestionNumber));
+        mButtonChoice2.setText(mQuestionLibrary.getChoice2(mQuestionNumber));
+        mButtonChoice3.setText(mQuestionLibrary.getChoice3(mQuestionNumber));
+
+        mAnswer = mQuestionLibrary.getCorrectAnswer(mQuestionNumber);
+        mQuestionNumber++;
+    }
+
+
+    private void updateScore(int point) {
+        mScoreView.setText("" + mScore);
+
+    }
+
+}
